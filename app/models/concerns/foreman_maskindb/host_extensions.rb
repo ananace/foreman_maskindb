b9e9069ea75881be6236d4c1ccfde557ac34e6ff
Alexander Olofsson <alexander.olofsson@liu.se>
2017-08-23 16:03:33 +0200

    Support hardware information

--- app/models/concerns/foreman_maskindb/host_extensions.rb
+++ app/models/concerns/foreman_maskindb/host_extensions.rb
@@ -5,10 +5,44 @@ module ForemanMaskindb
     def maskindb_entry
       Rails.cache.fetch("#{cache_key}/maskindb_entry", expires_in: 15.minutes) do
         begin
+          hrd = JSON.parse(
+            RestClient::Request.execute(
+              method: :get,
+              url: "#{SETTINGS[:maskindb_url]}/api/hardware/#{name}/",
+              user: SETTINGS[:maskindb_user],
+              password: SETTINGS[:maskindb_password],
+              headers: {
+                content_type: 'application/json',
+                accept: 'application/json',
+                accept_charset: 'utf-8'
+              }
+            ).body, symbolize_names: true
+          )
+
+          %i(datacenter rack model).each do |key|
+            next unless hrd[key]
+
+            data = JSON.parse(
+              RestClient::Request.execute(
+                method: :get,
+                url: hrd[key],
+                user: SETTINGS[:maskindb_user],
+                password: SETTINGS[:maskindb_password],
+                headers: {
+                  content_type: 'application/json',
+                  accept: 'application/json',
+                  accept_charset: 'utf-8'
+                }
+              ).body, symbolize_names: true
+            )
+
+            hrd[key] = data.reject { |k, _v| k == :url }.deep_symbolize_keys
+          end
+
           srv = JSON.parse(
             RestClient::Request.execute(
               method: :get,
-              url: "#{SETTINGS[:maskindb_url]}/api/servers/#{name}/",
+              url: hrd[:server],
               user: SETTINGS[:maskindb_user],
               password: SETTINGS[:maskindb_password],
               headers: {
@@ -39,7 +73,9 @@ module ForemanMaskindb
             srv[key] = data.reject { |k, _v| k == :url }.deep_symbolize_keys
           end
 
-          srv
+          hrd.delete :server
+          hrd.delete :url
+          hrd.merge srv
         rescue RestClient::NotFound
           nil
         end
