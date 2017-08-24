8aef895a4a861674eadaf8aadea76f7401360620
Alexander Olofsson <alexander.olofsson@liu.se>
2017-08-24 08:56:56 +0200

    De-duplicating code

--- app/models/concerns/foreman_maskindb/host_extensions.rb
+++ app/models/concerns/foreman_maskindb/host_extensions.rb
@@ -3,73 +3,40 @@ module ForemanMaskindb
     extend ActiveSupport::Concern
 
     def maskindb_entry
+      mdb_query = proc do |url|
+        logger.debug "Rest call to #{url}..."
+        data = JSON.parse(
+          RestClient::Request.execute(
+            method: :get,
+            url: url,
+            user: SETTINGS[:maskindb_user],
+            password: SETTINGS[:maskindb_password],
+            headers: {
+              content_type: 'application/json',
+              accept: 'application/json',
+              accept_charset: 'utf-8'
+            }
+          ).body, symbolize_names: true
+        )
+        logger.debug "#{url} => #{data}"
+        data
+      end.freeze
+
       Rails.cache.fetch("#{cache_key}/maskindb_entry", expires_in: 15.minutes) do
         begin
-          hrd = JSON.parse(
-            RestClient::Request.execute(
-              method: :get,
-              url: "#{SETTINGS[:maskindb_url]}/api/hardware/#{name}/",
-              user: SETTINGS[:maskindb_user],
-              password: SETTINGS[:maskindb_password],
-              headers: {
-                content_type: 'application/json',
-                accept: 'application/json',
-                accept_charset: 'utf-8'
-              }
-            ).body, symbolize_names: true
-          )
-
+          hrd = mdb_query.call "#{SETTINGS[:maskindb_url]}/api/hardware/#{name}/"
           %i(datacenter rack model).each do |key|
             next unless hrd[key]
 
-            data = JSON.parse(
-              RestClient::Request.execute(
-                method: :get,
-                url: hrd[key],
-                user: SETTINGS[:maskindb_user],
-                password: SETTINGS[:maskindb_password],
-                headers: {
-                  content_type: 'application/json',
-                  accept: 'application/json',
-                  accept_charset: 'utf-8'
-                }
-              ).body, symbolize_names: true
-            )
-
+            data = mdb_query.call hrd[key]
             hrd[key] = data.reject { |k, _v| k == :url }.deep_symbolize_keys
           end
 
-          srv = JSON.parse(
-            RestClient::Request.execute(
-              method: :get,
-              url: hrd[:server],
-              user: SETTINGS[:maskindb_user],
-              password: SETTINGS[:maskindb_password],
-              headers: {
-                content_type: 'application/json',
-                accept: 'application/json',
-                accept_charset: 'utf-8'
-              }
-            ).body, symbolize_names: true
-          )
-
+          srv = mdb_query.call hrd[:server]
           %i(status admin backupadmin group).each do |key|
             next unless srv[key]
 
-            data = JSON.parse(
-              RestClient::Request.execute(
-                method: :get,
-                url: srv[key],
-                user: SETTINGS[:maskindb_user],
-                password: SETTINGS[:maskindb_password],
-                headers: {
-                  content_type: 'application/json',
-                  accept: 'application/json',
-                  accept_charset: 'utf-8'
-                }
-              ).body, symbolize_names: true
-            )
-
+            data = mdb_query.call srv[key]
             srv[key] = data.reject { |k, _v| k == :url }.deep_symbolize_keys
           end
 
