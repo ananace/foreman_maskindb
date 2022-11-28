055fdd57860e2de059e0537e12731ee461b32a6d
Alexander Olofsson <alexander.olofsson@liu.se>
2022-11-28 16:32:46 +0100

    Final test fixing

--- test/unit/host_test.rb
+++ test/unit/host_test.rb
@@ -12,10 +12,37 @@ class HostTest < ActiveSupport::TestCase
 
   context 'With MaskinDB entry' do
     test 'it should retrieve the MaskinDB data' do
-      host.expects(:mdb_query).with('http://example.com/api/hardware/example').once.returns '{"rack":"http://example.com/api/rack/example"}'
-      host.expects(:mdb_query).with('http://example.com/api/rack/example').once.returns '{"name":"Example"}'
+      hardware_data = {
+        rack: "#{SETTINGS[:maskindb_url]}/api/rack/example/",
+        model: "#{SETTINGS[:maskindb_url]}/api/model/example/",
+        server: "#{SETTINGS[:maskindb_url]}/api/servers/#{host.name}/"
+      }
+      rack_data = {
+        name: 'Example'
+      }
+      model_data = {
+        name: 'Example'
+      }
+      server_data = {
+        name: 'Example',
+        status: "#{SETTINGS[:maskindb_url]}/api/status/example/"
+      }
+      status_data = {
+        name: 'Active'
+      }
 
-      assert_equal({ rack: { name: 'Example' } }, host.maskindb_entry)
+      expected_data = {
+        rack: rack_data,
+        model: model_data,
+      }.merge(server_data).merge(status: status_data)
+
+      host.expects(:mdb_query).with("http://example.com/api/hardware/#{host.name}/").once.returns hardware_data
+      host.expects(:mdb_query).with('http://example.com/api/rack/example/').once.returns rack_data
+      host.expects(:mdb_query).with('http://example.com/api/model/example/').once.returns model_data
+      host.expects(:mdb_query).with("http://example.com/api/servers/#{host.name}/").once.returns server_data
+      host.expects(:mdb_query).with('http://example.com/api/status/example/').once.returns status_data
+
+      assert_equal expected_data, host.maskindb_entry
     end
   end
 
@@ -23,7 +50,7 @@ class HostTest < ActiveSupport::TestCase
     test 'it should handle failing to retrieve the MaskinDB data' do
       host
         .expects(:mdb_query)
-        .with('http://example.com/api/hardware/example')
+        .with("http://example.com/api/hardware/#{host.name}/")
         .once
         .raises RestClient::Exceptions::EXCEPTIONS_MAP[404]
 
